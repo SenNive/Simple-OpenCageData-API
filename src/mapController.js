94b45fcdc856edf556ee539701a6d7f87214127c
Kevin Yohanes <kvnyhns@gmail.com>
2024-01-16 01:24:42 +0700

    Refactor mapController.js: Import sendErrorResponse and sendSuccessResponse

--- src/mapController.js
+++ src/mapController.js
@@ -1,33 +1,9 @@
 const db = require('./db')
 const axios = require('axios')
 const geolib = require('geolib')
-const convert = require('xml-js');
+const { sendErrorResponse, sendSuccessResponse } = require('./helper/sendRequest')
 
 const apiKey = '***REMOVED***'
-
-const HTTP_STATUS_CODES = require('./HTTP_STATUS_CODES.js')
-
-const convertJsonToXml = (json) => {
-  return convert.json2xml(json, { compact: true, spaces: 4 });
-};
-
-const sendErrorResponse = (req, res, status, message = "") => {
-  const errorResponse = { message: `${HTTP_STATUS_CODES[status]}${message ? ' ' + message : ''}` };
-  sendResponse(req, res, status, errorResponse);
-};
-
-const sendSuccessResponse = (req, res, status, data) => {
-  sendResponse(req, res, status, data);
-};
-
-const sendResponse = (req, res, status, payload) => {
-  if (req.headers.accept === 'application/xml') {
-    const result = convertJsonToXml(payload);
-    res.status(status).type('application/xml').send(result);
-  } else {
-    res.status(status).json(payload);
-  }
-};
 
 const authenticate = (req, res, next) => {
   const username = req.query.username;
