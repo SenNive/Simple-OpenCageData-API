61dacb96ebb21626d8d4850cf5fdbaf69164da41
Kevin Yohanes <kvnyhns@gmail.com>
2024-01-16 01:11:13 +0700

    Update HTTP status codes and descriptions

--- src/HTTP_STATUS_CODES.js
+++ src/HTTP_STATUS_CODES.js
@@ -1,9 +1,11 @@
 const HTTP_STATUS_CODES = {
     200: 'Success.',
-    400: 'Bad Request. Harap isi semua kolom yang diperlukan.',
-    401: 'Autentikasi gagal.',
-    404: 'Not Found. Alamat tidak ditemukan.',
+    201: 'Created.',
+    400: 'Bad Request.',
+    401: 'Unauthorized.',
+    404: 'Not Found.',
     406: 'Not Acceptable.',
+    409: 'Conflict.',
     500: 'Internal Server Error.',
 };
 
