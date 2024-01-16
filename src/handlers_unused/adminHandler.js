777aa89baec75e736b2cb82483597c3331174da4
Kevin Yohanes <kvnyhns@gmail.com>
2024-01-16 23:09:02 +0700

    Remove unused files.

---
+++ src/handlers_unused/adminHandler.js
@@ -0,0 +1,24 @@
+// const adminController = require('../controllers/adminController')
+
+// module.exports = app => {
+//   app.get(
+//     '/admin/users',
+//     adminController.authenticate,
+//     adminController.getUsers
+//   )
+//   app.post(
+//     '/admin/users',
+//     adminController.authenticate,
+//     adminController.addUser
+//   )
+//   app.put(
+//     '/admin/users',
+//     adminController.authenticate,
+//     adminController.editUser
+//   )
+//   app.delete(
+//     '/admin/users',
+//     adminController.authenticate,
+//     adminController.deleteUser
+//   )
+// }
---
+++ src/handlers_unused/mapHandler.js
@@ -0,0 +1,12 @@
+// const mapController = require('../controllers/mapController')
+
+// module.exports = app => {
+//   app.get('/map/worldwide', mapController.authenticate, mapController.worldwide)
+//   app.get('/map/reverse', mapController.authenticate, mapController.reverse)
+//   app.get('/map/forward', mapController.authenticate, mapController.forward)
+//   app.get(
+//     '/map/calculateDistance',
+//     mapController.authenticate,
+//     mapController.calculateDistance
+//   )
+// }
